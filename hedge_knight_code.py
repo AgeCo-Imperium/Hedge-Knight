2f63fd77de8f730f78e471b9d14ad3f422f5ab2b
AgeCo-Imperium <59127912+AgeCo-Imperium@users.noreply.github.com>
2019-12-30 18:04:33 -0500

    Add files via upload

--- hedge_knight_code.py
+++ hedge_knight_code.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 #This makes it so typing hk.commandname makes the bot perform a specific command
@@ -17,26 +18,21 @@ async def on_ready():
 
 #New Event, prints that's a bruh moment when you type hk.bruhthem
 @client.command()
-async def bruhthem(ctx):
+async def bruh(ctx):
     
     await ctx.send("That's a bruh moment...")
     
 #End of Event
 
 
-
-#New Event, when someone types trumpdog the bot will print the emote (THIS DOES NOT WORK YET)
+    
+#New Event, prints oh wow if someone says wow
 @client.event
 async def on_message(message):
 
-    #This line makes sure the bot does not respond to its own message
-    if client.user.id != message.author.id:
-        #If trumpdog is somewhere in the message await for the bot to send a given message.
-        if "trumpdog" in message.content:
-            await channel.send("<:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441>")
-
-    await client.process_commands(message)   
-
+    if "trumpdog" in message.content:
+        await message.channel.send("<:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441> <:TrumpDog:639548018231869441>")
+                               
 #End of Event
 
 
